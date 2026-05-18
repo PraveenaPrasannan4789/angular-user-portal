0396d10f956cfb4f76b8a7c3eb96f522b67242a1
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-05-18 23:13:36 +0100

    api added

--- client/src/app/user.ts
+++ client/src/app/user.ts
@@ -1,12 +1,27 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class UserService {
+//   users = ['Praveena', 'John', 'Sarah'];
+
+//   getUsers() {
+//     return this.users;
+//   }
+// }
+
+//api used
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  users = ['Praveena', 'John', 'Sarah'];
+  constructor(private http: HttpClient) {}
 
   getUsers() {
-    return this.users;
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 }
